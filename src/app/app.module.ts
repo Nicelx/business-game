802aa4c048e73d3fcad540246c800631b6d585a3
Nicelx <relush321@gmail.com>
2022-11-27 23:56:54 +0300

    generate game component, try to navigate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LobbyComponent } from './lobby/lobby.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LobbyComponent
+    LobbyComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
