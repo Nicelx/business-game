78ee23f71e2fc089b0c26c028e5c74b7776c6605
Nicelx <relush321@gmail.com>
2022-11-29 23:55:45 +0300

    test-like navigation load, game component view markup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; 
+import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 
 const routes: Routes = [
-  {path: 'game/', component: GameComponent }
+  { path: "", component : AppComponent, pathMatch: "full" },
+  {path: 'game', component: GameComponent }
 ]; 
 
 @NgModule({
