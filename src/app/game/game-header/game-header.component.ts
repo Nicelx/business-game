02a60ab77eacbb4e0dfa24f36ef0d4059a6478c8
Nicelx <relush321@gmail.com>
2022-12-17 23:56:59 +0300

    player things

--- src/app/game/game-header/game-header.component.ts
+++ src/app/game/game-header/game-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerData } from '../interfaces/game.interfaces';
+import { PlayerDataService } from '../services/player-data.service';
 
 @Component({
   selector: 'app-game-header',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-header.component.css']
 })
 export class GameHeaderComponent implements OnInit {
-
-  constructor() { }
+  player: PlayerData = {
+    playerId: 0,
+    playerName: '',
+    money: 0,
+    businessUnits: []
+  }
+  constructor(private playerService: PlayerDataService) { }
 
   ngOnInit(): void {
+    this.player = this.playerService.getPlayer(0);
   }
 
 }
--- src/app/game/services/player-data.service.ts
+++ src/app/game/services/player-data.service.ts
@@ -2,24 +2,27 @@ import { Injectable } from "@angular/core";
 import { BusinessUnit, PlayerData } from "./../interfaces/game.interfaces";
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: "root",
 })
 export class PlayerDataService {
-	constructor(private data: PlayerData|null) {
-		data = getPlayer()
-	}
+	constructor() {}
 
 	playersData: PlayerData[] = [
-		{ playerId: 0, playerName: "player", money: 0, businessUnits: [] },
+		{ playerId: 0, playerName: "player", money: 0, businessUnits: [{
+			unitId: 0,
+			type: 'apples'
+		}] },
 	];
 
-	addBusinessUnit(unit: BusinessUnit) {
-		this.data.businessUnits.push(unit);
+	addBusinessUnit(unit: BusinessUnit, playerId: number) {
+		const player = this.getPlayer(playerId);
+		if (!player) return;
+		player.businessUnits.push(unit);
 	}
 
 	getPlayer(playerId: number) {
 		const player = this.playersData.find((item) => item.playerId === playerId);
-		if (!player) return;
-		return player.money;
+		if (!player) throw new Error('Player doesnt exist');
+		return player;
 	}
 }
