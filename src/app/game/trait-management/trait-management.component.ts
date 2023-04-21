87533932be0fd233d68946141de5f10e1af1ae53
Nicelx <relush321@gmail.com>
2023-04-21 23:54:07 +0300

    move some traits

--- src/app/game/trait-management/trait-management.component.ts
+++ src/app/game/trait-management/trait-management.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-trait-management',
-  templateUrl: './trait-management.component.html',
-  styleUrls: ['./trait-management.component.css']
+	selector: "app-trait-management",
+	templateUrl: "./trait-management.component.html",
+	styleUrls: ["./trait-management.component.css"],
 })
 export class TraitManagementComponent implements OnInit {
+	traits = [];
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	onAddTrait() {
+		let buyingBool = this.playerService.buyTrait(this.traitSelected, 0, 1);
+		if (buyingBool) this.traitCost = this.traitService.getTraitCost(this.traitSelected);
+
+		// to do implement check
+		this.isAddCheck = true;
+	}
+	onImproveTrait() {
+		let trait = this.traitService.checkTrait(this.traitSelected);
+		if (!trait) return;
+		this.playerService.buyTrait(this.traitSelected, 0, trait.level + 1);
+		this.traitCost = this.traitService.getTraitCost(this.traitSelected);
+	}
+
+	onTraitSelection(value: any) {
+		this.traitSelected = value;
+		this.traitCost = this.traitService.getTraitCost(this.traitSelected);
+		if (this.traitService.checkTrait(this.traitSelected)) {
+			this.isAddCheck = true;
+		} else {
+			this.isAddCheck = false;
+		}
+	}
 }
