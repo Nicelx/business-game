3dcfdb117c11eebcc6b0bde899900a5fbbec501d
Nicelx <relush321@gmail.com>
2023-05-15 00:00:38 +0300

    think bots logic, have one good simple idea

--- src/app/game/services/bots-service.ts
+++ src/app/game/services/bots-service.ts
@@ -1,13 +1,54 @@
 import { Injectable } from "@angular/core";
 import { PlayerDataService } from "./player-data.service";
 import { PlayerData } from "../interfaces/game.interfaces";
+import { unitType } from "../interfaces/game.interfaces";
+
+const variator = {
+	generalAi: {
+		
+
+		// types: [
+		// 	{
+		// 		type: "apples",
+		// 		chance: 1,
+		// 	},
+		// 	{
+		// 		type: "iron",
+		// 		chance: 1,
+		// 	},
+		// 	{
+		// 		type: "juice",
+		// 		chance: 1,
+		// 	},
+		// ],
+		// typesChancesSum: 3,
+		// traits: [
+		// 	{
+		// 		trait: "RevenuePlus",
+		// 		chance: 1,
+		// 	},
+		// 	{
+		// 		trait: "ExpensesPlus",
+		// 		chance: 1,
+		// 	},
+		// 	{
+		// 		trait: "IncomePlus",
+		// 		chance: 1,
+		// 	},
+		// 	{
+		// 		trait: "CheapBuilding",
+		// 		chance: 1,
+		// 	},
+		// ],
+	},
+};
 
 @Injectable({
-	providedIn: "root"
+	providedIn: "root",
 })
 export class BotsService {
 	constructor() {
-		this.initBotsLogic()
+		this.initBotsLogic();
 	}
 
 	bots: PlayerData[] = [
@@ -18,14 +59,14 @@ export class BotsService {
 			businessUnits: [
 				{
 					unitId: 1,
-				sellingType: 'retail', 
-				type: ['apples'],
-				earned: 0,
-				amount: 1,
-				incomePerTick: 0,
-				expensePerTick: 0,
-				revenuePerTick: 0,
-				}
+					sellingType: "retail",
+					type: ["apples"],
+					earned: 0,
+					amount: 1,
+					incomePerTick: 0,
+					expensePerTick: 0,
+					revenuePerTick: 0,
+				},
 			],
 			playerIncomePerTick: 0,
 			traits: [],
@@ -33,10 +74,12 @@ export class BotsService {
 	];
 
 	private initBotsLogic() {
-		setInterval(() => {this.bots[0].businessUnits[0].amount++}, 1000)
+		setInterval(() => {
+			this.bots[0].businessUnits[0].amount++;
+		}, 1000);
 	}
 
 	public getBots() {
-		return this.bots
+		return this.bots;
 	}
 }
